6abe80fcbb1378b5f773a2ffdeee0209a775c02a
KhaLe <minhkhanb@gmail.com>
2019-04-03 16:10:20 +0700

    check components

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld/HelloWorld.vue'
+import HelloWorld from '@/components/Home/Home'
 
 Vue.use(Router)
 
